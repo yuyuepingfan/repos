6bfe952444671f7c7889d9f4a233c50a45382e91
yuyuepingfan <739180483@qq.com>
2018-02-10 20:09:07 +0800

    first commit

---
+++ Common/Common.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifdef  _DEBUG
+#pragma comment(lib, "osgd.lib")
+#pragma comment(lib, "osgDBd.lib")
+#pragma comment(lib, "osgViewerd.lib")
+#pragma comment(lib, "OpenThreadsd.lib")
+#pragma comment(lib, "osgGAd.lib")
+#endif // !_DEBUG
+
---
+++ Common/main.cpp
@@ -0,0 +1,6 @@
+#include <QtCore/QCoreApplication>
+#include "Common.h"
+int main(int argc, char *argv[])
+{
+	return 0;
+}
